ea40ba21a939679b48cee22825c5c7f219e3fde4
Quantz <quanhongdagm@gmail.com>
2022-08-07 11:39:25 +0800

    Movement Class to real move

--- Package/Movement.py
+++ Package/Movement.py
@@ -1,4 +1,3 @@
-from Package.GPS_Package import get_gps_data, draw_map
 import serial
 
 
@@ -9,28 +8,51 @@ class Movement:
         # ser = serial.Serial("/dev/ttyUSB0", 115200, timeout=0.5)
         if self.ser.isOpen():
             self.ser.close()
-        self.ser.open()
 
     def forward(self):
+        self.ser.open()
         self.ser.write(b"$CTRL11\n")
+        self.ser.flushOutput()
+        self.ser.close()
 
     def backward(self):
+        self.ser.open()
         self.ser.write(b"$CTRL00\n")
+        self.ser.flushOutput()
+        self.ser.close()
 
     def left(self):
+        self.ser.open()
         self.ser.write(b"$CTRL10\n")
+        self.ser.flushOutput()
+        self.ser.close()
 
     def right(self):
+        self.ser.open()
         self.ser.write(b"$CTRL01\n")
+        self.ser.flushOutput()
+        self.ser.close()
 
     def bump(self, num=None):
         if num is None:
             raise ValueError
         if num == 0:
+            self.ser.open()
             self.ser.write(b"$BUMP00\n")
+            self.ser.flushOutput()
+            self.ser.close()
         if num == 1:
+            self.ser.open()
             self.ser.write(b"$BUMP01\n")
+            self.ser.flushOutput()
+            self.ser.close()
         if num == 2:
+            self.ser.open()
             self.ser.write(b"$BUMP10\n")
+            self.ser.flushOutput()
+            self.ser.close()
         if num == 3:
+            self.ser.open()
             self.ser.write(b"$BUMP11\n")
+            self.ser.flushOutput()
+            self.ser.close()
